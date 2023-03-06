3bc6c4ab82157b7d3794a3256954b7deb86f7903
Paul Gooderham <turkeyonmarblerye@gmail.com>
2023-03-06 16:15:52 -0500

    Temporarily log JSONs. Handle server capability message for codeActions.
    Signed-off-by: Paul Gooderham <turkeyonmarblerye@gmail.com>

--- src/main/java/io/openliberty/tools/intellij/lsp4mp/lsp4ij/LanguageServerWrapper.java
+++ src/main/java/io/openliberty/tools/intellij/lsp4mp/lsp4ij/LanguageServerWrapper.java
@@ -29,53 +29,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.messages.MessageBusConnection;
 import io.openliberty.tools.intellij.lsp4mp.lsp4ij.server.StreamConnectionProvider;
-import org.eclipse.lsp4j.ClientCapabilities;
-import org.eclipse.lsp4j.ClientInfo;
-import org.eclipse.lsp4j.CodeActionCapabilities;
-import org.eclipse.lsp4j.CodeActionKind;
-import org.eclipse.lsp4j.CodeActionKindCapabilities;
-import org.eclipse.lsp4j.CodeActionLiteralSupportCapabilities;
-import org.eclipse.lsp4j.CodeLensCapabilities;
-import org.eclipse.lsp4j.ColorProviderCapabilities;
-import org.eclipse.lsp4j.CompletionCapabilities;
-import org.eclipse.lsp4j.CompletionItemCapabilities;
-import org.eclipse.lsp4j.DefinitionCapabilities;
-import org.eclipse.lsp4j.DidChangeWorkspaceFoldersParams;
-import org.eclipse.lsp4j.DocumentFormattingOptions;
-import org.eclipse.lsp4j.DocumentHighlightCapabilities;
-import org.eclipse.lsp4j.DocumentLinkCapabilities;
-import org.eclipse.lsp4j.DocumentRangeFormattingOptions;
-import org.eclipse.lsp4j.DocumentSymbolCapabilities;
-import org.eclipse.lsp4j.ExecuteCommandCapabilities;
-import org.eclipse.lsp4j.ExecuteCommandOptions;
-import org.eclipse.lsp4j.FailureHandlingKind;
-import org.eclipse.lsp4j.FormattingCapabilities;
-import org.eclipse.lsp4j.HoverCapabilities;
-import org.eclipse.lsp4j.InitializeParams;
-import org.eclipse.lsp4j.InitializedParams;
-import org.eclipse.lsp4j.MarkupKind;
-import org.eclipse.lsp4j.RangeFormattingCapabilities;
-import org.eclipse.lsp4j.ReferencesCapabilities;
-import org.eclipse.lsp4j.Registration;
-import org.eclipse.lsp4j.RegistrationParams;
-import org.eclipse.lsp4j.RenameCapabilities;
-import org.eclipse.lsp4j.ResourceOperationKind;
-import org.eclipse.lsp4j.ServerCapabilities;
-import org.eclipse.lsp4j.SignatureHelpCapabilities;
-import org.eclipse.lsp4j.SymbolCapabilities;
-import org.eclipse.lsp4j.SymbolKind;
-import org.eclipse.lsp4j.SymbolKindCapabilities;
-import org.eclipse.lsp4j.SynchronizationCapabilities;
-import org.eclipse.lsp4j.TextDocumentClientCapabilities;
-import org.eclipse.lsp4j.TextDocumentSyncKind;
-import org.eclipse.lsp4j.TextDocumentSyncOptions;
-import org.eclipse.lsp4j.TypeDefinitionCapabilities;
-import org.eclipse.lsp4j.UnregistrationParams;
-import org.eclipse.lsp4j.WorkspaceClientCapabilities;
-import org.eclipse.lsp4j.WorkspaceEditCapabilities;
-import org.eclipse.lsp4j.WorkspaceFoldersChangeEvent;
-import org.eclipse.lsp4j.WorkspaceFoldersOptions;
-import org.eclipse.lsp4j.WorkspaceServerCapabilities;
+import org.eclipse.lsp4j.*;
 import org.eclipse.lsp4j.jsonrpc.Launcher;
 import org.eclipse.lsp4j.jsonrpc.MessageConsumer;
 import org.eclipse.lsp4j.jsonrpc.ResponseErrorException;
@@ -391,9 +345,11 @@ public class LanguageServerWrapper {
                 && ((ResponseMessage) message).getId()
                 .equals(Integer.toString(ResponseErrorCode.RequestCancelled.getValue()))) {
             ResponseMessage responseMessage = (ResponseMessage) message;
-            LOGGER.warn("", new ResponseErrorException(responseMessage.getError()));
+            LOGGER.warn("logMessage:", new ResponseErrorException(responseMessage.getError()));
         } else if (LOGGER.isDebugEnabled()) {
             LOGGER.info(message.getClass().getSimpleName() + '\n' + message.toString());
+        } else {
+            LOGGER.warn(message.toString()); // TODO: Need to display JSON message?
         }
     }
 
@@ -804,7 +760,11 @@ public class LanguageServerWrapper {
                     serverCapabilities.setDocumentRangeFormattingProvider(documentRangeFormattingProvider.getRight());
                     addRegistration(reg, () -> serverCapabilities.setDocumentRangeFormattingProvider(documentRangeFormattingProvider ));
                 }
-            //} else if ("textDocument/codeAction".equals(reg.getMethod())) { //$NON-NLS-1$
+            } else if ("textDocument/codeAction".equals(reg.getMethod())) { //$NON-NLS-1$
+                final Either<Boolean, CodeActionOptions> beforeRegistration = serverCapabilities.getCodeActionProvider();
+                LOGGER.warn("LanguageServerWrapper textDocument/codeAction in getRegistrations()"); // TODO: delete
+                serverCapabilities.setCodeActionProvider(Boolean.TRUE);
+                addRegistration(reg, () -> serverCapabilities.setCodeActionProvider(beforeRegistration));
             }
         });
     }
