ad6059607c70c45ccb880f6eb0890c3efdd10683
Paul Gooderham <turkeyonmarblerye@gmail.com>
2023-06-14 18:38:47 -0400

    Refactor some of the packages per plugin version 1.20.1
    Signed-off-by: Paul Gooderham <turkeyonmarblerye@gmail.com>

---
+++ src/main/java/com/redhat/devtools/intellij/lsp/MicroProfileLanguageClient.java
@@ -0,0 +1,154 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at https://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.redhat.devtools.intellij.lsp;
+
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.libraries.Library;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.messages.MessageBusConnection;
+import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.ProjectLabelManager;
+import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.PropertiesManager;
+import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.PropertiesManagerForJava;
+import io.openliberty.tools.intellij.lsp4mp4ij.psi.core.project.PsiMicroProfileProjectManager;
+import io.openliberty.tools.intellij.lsp4mp.MicroProfileModuleUtil;
+import io.openliberty.tools.intellij.lsp4mp.MicroProfileProjectService;
+import com.redhat.devtools.intellij.lsp4ij.LanguageClientImpl;
+import io.openliberty.tools.intellij.lsp4mp4ij.psi.internal.core.ls.PsiUtilsLSImpl;
+import org.apache.commons.lang3.tuple.Pair;
+import org.eclipse.lsp4j.CodeAction;
+import org.eclipse.lsp4j.CodeLens;
+import org.eclipse.lsp4j.CompletionList;
+import org.eclipse.lsp4j.Hover;
+import org.eclipse.lsp4j.Location;
+import org.eclipse.lsp4j.PublishDiagnosticsParams;
+import org.eclipse.lsp4mp.commons.*;
+import org.eclipse.lsp4mp.commons.utils.JSONUtility;
+import org.eclipse.lsp4mp.ls.api.MicroProfileLanguageClientAPI;
+import org.eclipse.lsp4mp.ls.api.MicroProfileLanguageServerAPI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+/**
+ * Adapted from https://github.com/redhat-developer/intellij-quarkus/blob/2585eb422beeb69631076d2c39196d6eca2f5f2e/src/main/java/com/redhat/devtools/intellij/quarkus/lsp/QuarkusLanguageClient.java
+ * to start LSP4MP, Language Server for MicroProfile
+ */
+public class MicroProfileLanguageClient extends LanguageClientImpl implements MicroProfileLanguageClientAPI, MicroProfileProjectService.Listener {
+  private static final Logger LOGGER = LoggerFactory.getLogger(MicroProfileLanguageClient.class);
+  private static final String JAVA_FILE_EXTENSION = "java";
+
+  private final MessageBusConnection connection;
+
+  public MicroProfileLanguageClient(Project project) {
+    super(project);
+    connection = project.getMessageBus().connect(project);
+    connection.subscribe(MicroProfileProjectService.TOPIC, this);
+    MicroProfileProjectService.getInstance(project);
+  }
+
+  private void sendPropertiesChangeEvent(List<MicroProfilePropertiesScope> scope, Set<String> uris) {
+    MicroProfileLanguageServerAPI server = (MicroProfileLanguageServerAPI) getLanguageServer();
+    if (server != null) {
+      MicroProfilePropertiesChangeEvent event = new MicroProfilePropertiesChangeEvent();
+      event.setType(scope);
+      event.setProjectURIs(uris);
+      server.propertiesChanged(event);
+    }
+  }
+
+  @Override
+  public void libraryUpdated(Library library) {
+    sendPropertiesChangeEvent(Collections.singletonList(MicroProfilePropertiesScope.dependencies), MicroProfileModuleUtil.getModulesURIs(getProject()));
+  }
+
+  @Override
+  public void sourceUpdated(List<Pair<Module, VirtualFile>> sources) {
+    List<Pair<String,MicroProfilePropertiesScope>> info = sources.stream().
+            filter(pair -> isJavaFile(pair.getRight()) || isConfigSource(pair.getRight(), pair.getLeft())).
+            map(pair -> Pair.of(PsiUtilsLSImpl.getProjectURI(pair.getLeft()), getScope(pair.getRight()))).
+            collect(Collectors.toList());
+    if (!info.isEmpty()) {
+      sendPropertiesChangeEvent(info.stream().map(Pair::getRight).collect(Collectors.toList()), info.stream().map(Pair::getLeft).collect(Collectors.toSet()));
+    }
+  }
+
+  private MicroProfilePropertiesScope getScope(VirtualFile file) {
+    return isJavaFile(file)?MicroProfilePropertiesScope.sources:MicroProfilePropertiesScope.configfiles;
+  }
+
+  private boolean isJavaFile(VirtualFile file) {
+    return JAVA_FILE_EXTENSION.equals(file.getExtension());
+  }
+
+  private boolean isConfigSource(VirtualFile file, Module project) {
+    return PsiMicroProfileProjectManager.getInstance(project.getProject()).isConfigSource(file);
+  }
+
+  @Override
+  public CompletableFuture<MicroProfileProjectInfo> getProjectInfo(MicroProfileProjectInfoParams params) {
+    return runAsBackground("Computing project information", () -> PropertiesManager.getInstance().getMicroProfileProjectInfo(params, PsiUtilsLSImpl.getInstance(getProject())));
+  }
+
+  @Override
+  public CompletableFuture<Hover> getJavaHover(MicroProfileJavaHoverParams javaParams) {
+    return runAsBackground("Computing Java hover", () -> PropertiesManagerForJava.getInstance().hover(javaParams, PsiUtilsLSImpl.getInstance(getProject())));
+  }
+
+  @Override
+  public CompletableFuture<List<PublishDiagnosticsParams>> getJavaDiagnostics(MicroProfileJavaDiagnosticsParams javaParams) {
+    return runAsBackground("Computing Java diagnostics", () -> PropertiesManagerForJava.getInstance().diagnostics(javaParams, PsiUtilsLSImpl.getInstance(getProject())));
+  }
+
+  @Override
+  public CompletableFuture<Location> getPropertyDefinition(MicroProfilePropertyDefinitionParams params) {
+    return runAsBackground("Computing property definition", () -> PropertiesManager.getInstance().findPropertyLocation(params, PsiUtilsLSImpl.getInstance(getProject())));
+  }
+
+  @Override
+  public CompletableFuture<ProjectLabelInfoEntry> getJavaProjectlabels(MicroProfileJavaProjectLabelsParams javaParams) {
+    return runAsBackground("Computing Java projects labels", () -> ProjectLabelManager.getInstance().getProjectLabelInfo(javaParams, PsiUtilsLSImpl.getInstance(getProject())));
+  }
+
+  @Override
+  public CompletableFuture<JavaFileInfo> getJavaFileInfo(MicroProfileJavaFileInfoParams javaParams) {
+    return runAsBackground("Computing Java file info", () -> PropertiesManagerForJava.getInstance().fileInfo(javaParams, PsiUtilsLSImpl.getInstance(getProject())));
+  }
+
+  @Override
+  public CompletableFuture<CompletionList> getJavaCompletion(MicroProfileJavaCompletionParams javaParams) {
+    return runAsBackground("Computing Java completion", () -> PropertiesManagerForJava.getInstance().completion(javaParams, PsiUtilsLSImpl.getInstance(getProject())));
+  }
+
+  @Override
+  public CompletableFuture<List<? extends CodeLens>> getJavaCodelens(MicroProfileJavaCodeLensParams javaParams) {
+    return runAsBackground("Computing Java codelens", () -> PropertiesManagerForJava.getInstance().codeLens(javaParams, PsiUtilsLSImpl.getInstance(getProject())));
+  }
+  
+  @Override
+  public CompletableFuture<List<CodeAction>> getJavaCodeAction(MicroProfileJavaCodeActionParams javaParams) {
+    return runAsBackground("Computing Java code actions", () -> (List<CodeAction>) PropertiesManagerForJava.getInstance().codeAction(javaParams, PsiUtilsLSImpl.getInstance(getProject())));
+  }
+
+  @Override
+  public CompletableFuture<CodeAction> resolveCodeAction(CodeAction unresolved) {
+    return runAsBackground("Computing Java resolve code actions", () -> {
+      CodeActionResolveData data = JSONUtility.toModel(unresolved.getData(), CodeActionResolveData.class);
+      unresolved.setData(data);
+      return (CodeAction) PropertiesManagerForJava.getInstance().resolveCodeAction(unresolved, PsiUtilsLSImpl.getInstance(getProject()));
+    });
+  }
+}
--- src/main/java/com/redhat/devtools/intellij/lsp/MicroProfileServer.java
+++ src/main/java/com/redhat/devtools/intellij/lsp/MicroProfileServer.java
@@ -8,7 +8,7 @@
  * Contributors:
  * Red Hat, Inc. - initial API and implementation
  ******************************************************************************/
-package io.openliberty.tools.intellij.liberty.lsp;
+package com.redhat.devtools.intellij.lsp;
 
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginManagerCore;
@@ -20,51 +20,62 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.net.URI;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Start Liberty Language Server
  * Adapted from https://github.com/redhat-developer/intellij-quarkus/blob/2585eb422beeb69631076d2c39196d6eca2f5f2e/src/main/java/com/redhat/devtools/intellij/quarkus/lsp/QuarkusServer.java
+ * to start LSP4MP, Language Server for MicroProfile
  */
-public class LibertyConfigLanguageServer extends ProcessStreamConnectionProvider {
-    private static final Logger LOGGER = LoggerFactory.getLogger(LibertyConfigLanguageServer.class);
+public class MicroProfileServer extends ProcessStreamConnectionProvider {
 
-    public LibertyConfigLanguageServer() {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MicroProfileServer.class);
+
+    public MicroProfileServer() {
         IdeaPluginDescriptor descriptor = PluginManagerCore.getPlugin(PluginId.getId("open-liberty.intellij"));
-        File libertyServerPath = new File(descriptor.getPluginPath().toFile(), "lib/server/liberty-langserver-2.0-SNAPSHOT-jar-with-dependencies.jar");
+        File lsp4mpServerPath = new File(descriptor.getPluginPath().toFile(), "lib/server/org.eclipse.lsp4mp.ls-uber.jar");
         String javaHome = System.getProperty("java.home");
         if (javaHome == null) {
-            LOGGER.error("Unable to launch the Liberty language server. Could not resolve the java home system property");
+            LOGGER.error("Unable to launch the Eclipse LSP4MP language server. Could not resolve the java home system property");
             return;
         }
         if (!checkJavaVersion(javaHome, Constants.REQUIRED_JAVA_VERSION)) {
-            LOGGER.error("Unable to launch the Liberty language server." +
+            LOGGER.error("Unable to launch the Eclipse LSP4MP language server." +
                     " Java " + Constants.REQUIRED_JAVA_VERSION + " or more recent is required to run 'Liberty Tools for IntelliJ'." +
                     " Change the boot Java runtime of the IDE as documented here:" +
                     " https://www.jetbrains.com/help/idea/switching-boot-jdk.html");
             return;
         }
-        if (libertyServerPath.exists()) {
-            ArrayList<String> params = new ArrayList<>();
-            params.add(javaHome + File.separator + "bin" + File.separator + "java");
-
-            // Uncomment next line to attach debugger to LCLS at port 1064, debug params must come before -jar
-            // params.add("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1064");
-            params.add("-jar");
-            params.add(libertyServerPath.getAbsolutePath());
-            setCommands(params);
+        if (lsp4mpServerPath.exists()) {
+            setCommands(Arrays.asList(javaHome + File.separator + "bin" + File.separator + "java", "-jar",
+                    lsp4mpServerPath.getAbsolutePath(), "-DrunAsync=true"));
         } else {
-            LOGGER.warn(String.format("Unable to start the Liberty language server, Liberty language server path: %s does not exist", libertyServerPath));
+            LOGGER.warn(String.format("Unable to start Eclipse LSP4MP. Eclipse LSP4MP server path: %s does not exist"), lsp4mpServerPath);
         }
-
     }
 
     @Override
     public Object getInitializationOptions(URI rootUri) {
         Map<String, Object> root = new HashMap<>();
+        Map<String, Object> settings = new HashMap<>();
+        Map<String, Object> microprofile = new HashMap<>();
+        Map<String, Object> tools = new HashMap<>();
+        Map<String, Object> trace = new HashMap<>();
+        trace.put("server", "verbose");
+        tools.put("trace", trace);
+        Map<String, Object> codeLens = new HashMap<>();
+        codeLens.put("urlCodeLensEnabled", "true");
+        tools.put("codeLens", codeLens);
+        microprofile.put("tools", tools);
+        settings.put("microprofile", microprofile);
+        root.put("settings", settings);
         Map<String, Object> extendedClientCapabilities = new HashMap<>();
+        Map<String, Object> commands = new HashMap<>();
+        Map<String, Object> commandsKind = new HashMap<>();
+        commandsKind.put("valueSet", Arrays.asList(/*"microprofile.command.configuration.update",*/ "microprofile.command.open.uri"));
+        commands.put("commandsKind", commandsKind);
+        extendedClientCapabilities.put("commands", commands);
         extendedClientCapabilities.put("completion", new HashMap<>());
         extendedClientCapabilities.put("shouldLanguageServerExitOnShutdown", Boolean.TRUE);
         root.put("extendedClientCapabilities", extendedClientCapabilities);
